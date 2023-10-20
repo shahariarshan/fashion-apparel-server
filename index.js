ccdc3bbc113e155e77756e96415a8a12bbddb32f
unknown <shahariarmohammadhassan@gmail.com>
2023-10-20 19:54:18 +0600

    CURD get section completed

--- index.js
+++ index.js
@@ -109,18 +109,18 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+   
     // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-// CURD section 
+// CURD section   
 app.get('/', (req, res) => {
   res.send("Fashion  server is running soon")
 })
